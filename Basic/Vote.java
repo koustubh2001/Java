93e11d4139c23ddd50cd41618fb1d1fac841f435
koustubh <koustubhrayamane2001@gmail.com>
2025-01-02 17:03:51 +0530

    vote

--- Basic/Vote.java
+++ Basic/Vote.java
@@ -1,2 +1,13 @@
- 
- 
+
+ package Basic;
+ public class Vote {
+    public static void main(String[] args) {
+        int age = 18;  
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+    }
+}
