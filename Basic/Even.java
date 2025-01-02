c5d02c2e3e4283858e1cc8bded1c39edbd612eca
koustubh <koustubhrayamane2001@gmail.com>
2025-01-02 15:39:30 +0530

    vote

---
+++ Basic/Even.java
@@ -0,0 +1,12 @@
+package Basic;
+
+public class Even {
+
+    public static void main(String[] args) {
+        int num = 12;
+        if()
+        {
+            System.out.println();
+        }
+    }
+}
---
+++ Basic/Vote.java
@@ -0,0 +1,21 @@
+ package Basic;
+ public class Vote{
+    public static void main(String[] args) {
+        int age =99;
+        if (age<=18 ){
+            System.out.println("not vote");
+        }
+      
+       else if (age>=18 && age<=90){
+
+      
+            System.out.println(" vote");
+        }
+        else{
+
+            System.out.println("dead");
+        }
+
+    }
+}
+ 
