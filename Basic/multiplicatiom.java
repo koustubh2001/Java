ebc3633669cc2c50a1b8b064405ccb4592b60267
koustubh <koustubhrayamane2001@gmail.com>
2025-01-02 17:15:29 +0530

    5 multi

---
+++ Basic/multiplicatiom.java
@@ -0,0 +1,14 @@
+package Basic;
+
+public class multiplicatiom {
+    public static void main(String[] args) {
+        int num=5;
+        int result;
+        for(int i=1;i<=10;i++)
+        {
+            result=num*i;
+            System.out.println(num+" * "+i+" = "+result);
+        }
+    }
+    
+}
