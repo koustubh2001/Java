8e21fb2f179a4b7d20d2b56f957e8b06d7b0b60d
koustubh <koustubhrayamane2001@gmail.com>
2025-01-03 18:09:10 +0530

    Revers Array

---
+++ Basic/Revers.java
@@ -0,0 +1,12 @@
+package Basic;
+
+public class Revers {
+    public static void main(String[] args) {
+        int [] arr={1,2,3,4,5,6,7};
+        for(int i=arr.length-1;i>=1;i--)
+        {
+            System.out.print(i);
+        }
+    }
+    
+}
