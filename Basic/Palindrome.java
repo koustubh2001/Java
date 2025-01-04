659fb0875b9cfa41bae44c90abfaa626f18b9909
koustubh <koustubhrayamane2001@gmail.com>
2025-01-04 18:51:47 +0530

    first Commit

---
+++ Basic/Palindrome.java
@@ -0,0 +1,20 @@
+package Basic;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        String name="madam";
+        String rev="";
+        for(int i=name.length()-1;i>=0;i--)
+        {
+            rev=rev+name.charAt(i);
+        }
+        if(name.equals(rev))
+        {
+            System.out.println("Palindrome");
+        }
+        else
+        {
+            System.out.println("Not Palindrome");
+        }  
+    }   
+}
