8665a98bdbd1f3f46d59abec7410caf1065f1e0a
koustubh <koustubhrayamane2001@gmail.com>
2025-01-05 13:09:20 +0530

    First Commit

--- Basic/CountVowels.java
+++ Basic/CountVowels.java
@@ -1,11 +1,24 @@
 package Basic;
+import java.util.Scanner;
 
 public class CountVowels {
     public static void main(String[] args) {
-        String name="Koustubh";
+        Scanner scan=new Scanner (System.in);
+        System.out.println("Enter a Name:");
+       String name=scan.nextLine();
+        String str=name.toLowerCase();
+        int count=0;
+         for(int i=0;i<str.length();i++)
+         {
+                char ch=str.charAt(i);
+                if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+                {
+                    count++;
+                }
 
+         }
 
-
+         System.out.println( count);
 
     }
     
