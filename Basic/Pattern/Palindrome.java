1b7495a6e0f7e036e0a82108f397856576a85ced
koustubh <koustubhrayamane2001@gmail.com>
2025-01-10 20:10:41 +0530

    first commit

---
+++ Basic/Pattern/Palindrome.java
@@ -0,0 +1,25 @@
+package Basic.Pattern;
+
+public class Palindrome {
+    public static void main(String[] args) {
+       int n=5;
+       for(int i=1;i<=n;i++)
+       {
+        //space
+        for(int j=1;j<=n-i;j++)
+        {
+            System.out.print(" ");
+        }
+        for(int j=i;j>=1;j--)
+        {
+            System.out.print(j);
+        }
+        for(int j=2;j<=i;j++)
+        {
+            System.out.print(j);
+        }
+        System.out.println();
+       } 
+    }
+    
+}
