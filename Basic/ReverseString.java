dffbd7a400726dc370667515851504687380bcfb
koustubh <koustubhrayamane2001@gmail.com>
2025-01-12 20:08:16 +0530

    second commit

--- Basic/ReverseString.java
+++ Basic/ReverseString.java
@@ -8,6 +8,17 @@ public class ReverseString {
            System.out.print(name.charAt(i));
        
        }
-    } 
-        
-}
+
+       //////////////////////////////////////////////////  
+
+    //    public static void reverseString(String name) {
+    //    StringBuilder reversed = new StringBuilder(name);
+    //    System.out.println(reversed.reverse().toString());
+    //     }
+
+    // public static void main(String[] args) {
+    //     String name = "Koustubh";
+    //     reverseString(name); 
+    //     } 
+    
+}}
