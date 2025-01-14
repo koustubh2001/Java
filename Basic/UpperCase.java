6ad746d7e54fc46375a510c697cbbbe6fd4ceaee
koustubh <koustubhrayamane2001@gmail.com>
2025-01-14 17:09:57 +0530

    first commit

---
+++ Basic/UpperCase.java
@@ -0,0 +1,10 @@
+package Basic;
+
+public class UpperCase {
+    public static void main(String[] args) {
+       String name="hello";
+        String newname=name.toUpperCase();
+        System.out.println(newname); 
+    }
+    
+}
