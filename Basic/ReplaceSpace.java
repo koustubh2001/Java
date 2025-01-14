831b468d3c71d83808a1eff1eeb41a1dea262016
koustubh <koustubhrayamane2001@gmail.com>
2025-01-14 16:48:35 +0530

    first commit

---
+++ Basic/ReplaceSpace.java
@@ -0,0 +1,10 @@
+package Basic;
+
+public class ReplaceSpace {
+    public static void main(String[] args) {
+        String name = "hello world";
+        String newname = name.replace(' ', '-');
+        System.out.println(newname);
+    }
+    
+}
